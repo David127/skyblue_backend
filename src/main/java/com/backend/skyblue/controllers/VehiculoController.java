10cde9e97b6c3374bab16eea738ebcbf4e004e45
carlos <i201910263@cibertec.edu.pe>
2022-05-22 19:53:14 -0500

    update

--- src/main/java/com/backend/skyblue/controllers/VehiculoController.java
+++ src/main/java/com/backend/skyblue/controllers/VehiculoController.java
@@ -34,6 +34,7 @@ public class VehiculoController {
         SysHttpResponse response = new SysHttpResponse(HttpStatus.OK.value(), "Listado exitoso",listVehiculos);
         if(!asc)
             vehiculoService.listarEnPaginas( estado, PageRequest.of(page, size));
+            
         return  new ResponseEntity( response, HttpStatus.OK);
     }
 
