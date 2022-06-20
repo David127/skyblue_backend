d3d1e60104ea6b0e38304a54cc1844dc84fa1485
LuckySummer98 <alonso199037@gmail.com>
2022-06-19 21:39:23 -0500

    Se realizaron las validaciones

--- src/main/java/com/backend/skyblue/dto/request/VehiculoRequestDto.java
+++ src/main/java/com/backend/skyblue/dto/request/VehiculoRequestDto.java
@@ -3,6 +3,7 @@ package com.backend.skyblue.dto.request;
 import com.backend.skyblue.models.Ruta;
 import lombok.*;
 
+import javax.validation.constraints.NotNull;
 import java.io.Serial;
 import java.io.Serializable;
 
@@ -15,22 +16,31 @@ public class VehiculoRequestDto  implements Serializable {
     @Serial
     private static  final long serialVersionUID  = 1L;
     private Long id;
+    @NotNull(message = "La placa no puede ser vacio")
     private String placa;
+    @NotNull(message = "El padron no puede ser vacio")
     private String padron;
     private String clase;
+    @NotNull(message = "La marca no puede ser vacio")
     private String marca;
+    @NotNull(message = "El modelo no puede ser vacio")
     private String modelo;
     private String color;
+    @NotNull(message = "El tipo no puede ser vacio")
     private String tipo;
+    @NotNull(message = "La tarjeta de propiedad no puede ser vacio")
     private String tarjetaPropiedad;
     private String anioFabricacion;
     private String nroMoto;
     private Double pesoNeto;
     private Double pesoBruto;
     private Integer nroAsientos;
+    @NotNull(message = "El numero de pasajeros no puede ser vacio")
     private Integer nroPasajeros;
+    @NotNull(message = "el tipo de combustible no puede ser vacio")
     private Integer tipoCombustible;
     private String carroceria;
+    @NotNull(message = "El numero de serie no puede ser vacio")
     private String SerieMotor;
     private Integer nroCilindros;
     private Integer nroRuedas;
@@ -40,7 +50,7 @@ public class VehiculoRequestDto  implements Serializable {
     private Double cargaUtil;
     private Integer nroEjes;
     private Double kilomentraje;
-
+    @NotNull
     private RutaRequestDto ruta;
 
 }
